1694510b93c1a99255589f42ded663bdaaf3fadd
qcmiao1998 <qcmiao@hotmail.com>
2019-04-12 15:54:42 +0800

    Finish 5.5

--- Test/Chapter5.cs
+++ Test/Chapter5.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 using Introduction_to_Algorithms.Chapter_5;
 
@@ -20,6 +22,33 @@ namespace Test
         public void E5_4(int[] f, int n, int[] result)
         {
             var r = Example_5_4.Algorithm_Mapping(f, n);
+            Assert.Equal(result, r);
+        }
+
+
+        [Theory]
+        [InlineData("11111,01000,11001,11101,01010", 2)]
+        [InlineData("1011,1101,0011,0001", 4)]
+        [InlineData("1001,1101,0011,1001", 0)]
+        public void E5_5(string know, int result)
+        {
+            var ds1 = know.Split(',');
+            char[][] cs = new char[ds1.Length][];
+            for (int i = 0; i < ds1.Length; i++)
+            {
+                cs[i] = ds1[i].ToCharArray();
+            }
+
+            bool[,] bs = new bool[ds1.Length, ds1.Length];
+            for (int i = 0; i < ds1.Length; i++)
+            {
+                for (int j = 0; j < ds1.Length; j++)
+                {
+                    bs[i, j] = cs[i][j] == '1';
+                }
+            }
+
+            var r = Example_5_5.Algorithm_Celebrity(bs);
             Assert.Equal(result, r);
         }
 
